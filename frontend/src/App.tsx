1d2683780d31f8a09fb77c085abf1b37a879b4f5
render <142716427+render217@users.noreply.github.com>
2024-09-19 22:23:51 +0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+function App() {
+    return (
+        <Box>
+            <Typography variant="h1" sx={{ textAlign: "center" }}>
+                Hello World
+            </Typography>
+        </Box>
+    );
+}
+
+export default App;
