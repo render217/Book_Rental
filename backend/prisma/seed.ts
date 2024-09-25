7ee3fd4a9b4e804f099e1f752aa2eb1fb9168809
render <142716427+render217@users.noreply.github.com>
2024-09-26 01:00:34 +0300

    seed data

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, ApprovalStatus } from "@prisma/client";
+import { PrismaClient, ApprovalStatus, Role } from "@prisma/client";
 import { bookJSON, bookSeedData } from "./db";
 const prisma = new PrismaClient();
 
@@ -41,4 +41,34 @@ async function seedBooks() {
     }
 }
 
+async function seedAdmin() {
+    try {
+        const existingAdmin = await prisma.account.findFirst({
+            where: {
+                role: Role.ADMIN,
+                email: "admin@gmail.com",
+            },
+        });
+        if (existingAdmin) {
+            console.log("Admin account already exists. Skipping seeding.");
+            return;
+        }
+        const account = await prisma.account.create({
+            data: {
+                username: "admin",
+                password: "admin@123",
+                email: "admin@gmail.com",
+                role: Role.ADMIN,
+                location: "Admin Location",
+                phoneNumber: "1234567890",
+            },
+        });
+        console.log("Admin account created:", account);
+    } catch (error) {
+        console.error("Error seeding admin:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+seedAdmin();
 seedBooks();
